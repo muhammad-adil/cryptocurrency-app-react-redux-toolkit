03dd62619634caf9e8a612560f6cf55cefa77af6
muhammad-adil <adil.muhammad93@gmail.com>
2024-02-02 11:11:00 +0100

    Cryptocurrency App layout work

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space, Menu, Breadcrumb } from 'antd'
+import 'antd/dist/antd.css'
+
+
+
+function App() {
+    return (
+        <div className='app'>
+            <div className='navbar'></div>
+            {/* main section */}
+            <div className='main'>
+
+            </div>
+            <div className='footer'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default App
