ce31447150bb422cf8a9b48052f7ad42bbff98b9
muhammad-adil <adil.muhammad93@gmail.com>
2024-02-03 11:11:00 +0100

    Redux toolkit and api request work

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,14 +3,27 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+// useGetCryptosQuery from services
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Title } = Typography;
 
 const Homepage = () => {
+  // 
+  const { data, isFetching } = useGetCryptosQuery(10);
+  console.log(data);
 
   return (
     <>
-    <h1>Homepage</h1>
+       <Title level={2} className="heading">Global Crypto Stats</Title>
+       <Row gutter={[32, 32]}>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
+        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
+        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+      </Row>
     </>
   );
 };
