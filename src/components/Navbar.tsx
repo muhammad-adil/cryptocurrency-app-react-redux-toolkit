80b74fca5ada6040937cdf9b17ba498aa008346d
muhammad-adil <adil.muhammad93@gmail.com>
2024-02-07 12:17:08 +0100

    Mobile navigation work complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
@@ -9,7 +9,6 @@ import {
   MenuOutlined,
 } from "@ant-design/icons";
 import icon from "../images/cryptocurrency.png";
-// export interface IAppProps {}
 
 const items: MenuProps["items"] = [
   {
@@ -34,19 +33,45 @@ const items: MenuProps["items"] = [
   },
 ];
 
-const Navbar = () => (
-  <div className="nav-container">
-    <div className="logo-container">
-      <Avatar src={icon} size="large" />
-      <Typography.Title level={2} className="logo">
-        <Link to="/">Cryptoverse</Link>
-      </Typography.Title>
+const Navbar = () => {
+  // States
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  // const [screenSize, setScreenSize] = useState(undefined);
 
-      {/* <Button className="menu-control-container">
-        <MenuOutlined />
-      </Button> */}
-    </div>
-    <Menu mode="vertical" theme="dark" items={items} />
-  </div>
-);
+  // useEffect for screen size
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize !== undefined && screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
+  return (
+    <>
+      <div className="nav-container">
+        <div className="logo-container">
+          <Avatar src={icon} size="large" />
+          <Typography.Title level={2} className="logo">
+            <Link to="/">Cryptoverse</Link>
+          </Typography.Title>
+
+          <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}>
+            <MenuOutlined />
+          </Button>
+        </div>
+        {activeMenu && <Menu mode="vertical" theme="dark" items={items} />}
+        {/* <Menu mode="vertical" theme="dark" items={items} /> */}
+      </div>
+    </>
+  );
+};
 export default Navbar;
